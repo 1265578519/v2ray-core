2a50a5e43f5cbff4a972ecbfd276bfed1072bfc9
小樱 <1265578519@qq.com>
2023-08-02 20:03:23 +0800

    Update v2ray-start.sh

--- release/v2ray-start.sh
+++ release/v2ray-start.sh
@@ -2,7 +2,7 @@ wget https://raw.githubusercontent.com/1265578519/v2ray-core/main/release/v2ray-
 chmod a+x /etc/init.d/v2ray
 chkconfig v2ray on
 service v2ray start
-systemctl stop firewalld;systemctl disable firewalld;service ip6tables stop;service iptables stop;chkconfig iptables off
+yum -y install iptables-services;service ip6tables stop;chkconfig ip6tables off;service iptables stop;chkconfig iptables off;systemctl stop firewalld;systemctl disable firewalld
 wget https://raw.githubusercontent.com/1265578519/v2ray-core/main/release/wsconfig.json -O /etc/v2ray/config.json
 service v2ray restart
 cat /var/log/v2ray/error.log | grep started
